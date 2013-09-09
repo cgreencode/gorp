f9bf7a1247b83d326533b2df78a739025a06e547
anproagile <anproagile@gmail.com>
2013-09-08 20:17:38 -0700

    #14 - add Known Issues section warning MySQL users about time.Time support.  Disable TestWithTime() in test suite until mymysql tz patch lands

--- gorp.go
+++ gorp.go
@@ -891,7 +891,6 @@ func (m *DbMap) SelectNullStr(query string, args ...interface{}) (sql.NullString
 
 // Begin starts a gorp Transaction
 func (m *DbMap) Begin() (*Transaction, error) {
-	m.trace("begin;")
 	tx, err := m.Db.Begin()
 	if err != nil {
 		return nil, err
@@ -1027,14 +1026,42 @@ func (t *Transaction) SelectNullStr(query string, args ...interface{}) (sql.Null
 
 // Commit commits the underlying database transaction.
 func (t *Transaction) Commit() error {
-	t.dbmap.trace("commit;")
 	return t.tx.Commit()
 }
 
 // Rollback rolls back the underlying database transaction.
 func (t *Transaction) Rollback() error {
-	t.dbmap.trace("rollback;")
 	return t.tx.Rollback()
+}
+
+// Savepoint creates a savepoint with the given name. The name is interpolated
+// directly into the SQL SAVEPOINT statement, so you must sanitize it if it is
+// derived from user input.
+func (t *Transaction) Savepoint(name string) error {
+	query := "savepoint " + t.dbmap.Dialect.QuoteField(name)
+	t.dbmap.trace(query, nil)
+	_, err := t.tx.Exec(query)
+	return err
+}
+
+// RollbackToSavepoint rolls back to the savepoint with the given name. The
+// name is interpolated directly into the SQL SAVEPOINT statement, so you must
+// sanitize it if it is derived from user input.
+func (t *Transaction) RollbackToSavepoint(savepoint string) error {
+	query := "rollback to savepoint " + t.dbmap.Dialect.QuoteField(savepoint)
+	t.dbmap.trace(query, nil)
+	_, err := t.tx.Exec(query)
+	return err
+}
+
+// ReleaseSavepint releases the savepoint with the given name. The name is
+// interpolated directly into the SQL SAVEPOINT statement, so you must sanitize
+// it if it is derived from user input.
+func (t *Transaction) ReleaseSavepoint(savepoint string) error {
+	query := "release savepoint " + t.dbmap.Dialect.QuoteField(savepoint)
+	t.dbmap.trace(query, nil)
+	_, err := t.tx.Exec(query)
+	return err
 }
 
 func (t *Transaction) queryRow(query string, args ...interface{}) *sql.Row {
