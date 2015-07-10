f11d1a4ed27868b585741d645c91709672f02519
anproagile <anproagile@gmail.com>
2015-07-10 15:30:17 +0200

    Improve test_all.sh script. (changes by @rickb777, fixes #274)

--- dialect.go
+++ dialect.go
@@ -1,7 +1,6 @@
 package gorp
 
 import (
-	"errors"
 	"fmt"
 	"reflect"
 	"strings"
@@ -386,10 +385,10 @@ func (d MySQLDialect) ToSqlType(val reflect.Type, maxsize int, isAutoIncr bool) 
 	 * N is number of characters.
 	 * A varchar column can store up to 65535 bytes.
 	 * Remember that 1 character is 3 bytes in utf-8 charset.
-	 * Also remember that each row can store up to 65535 bytes, 
-	 * and you have some overheads, so it's not possible for a 
+	 * Also remember that each row can store up to 65535 bytes,
+	 * and you have some overheads, so it's not possible for a
 	 * varchar column to have 65535/3 characters really.
-	 * So it would be better to use 'text' type in stead of 
+	 * So it would be better to use 'text' type in stead of
 	 * large varchar type.
 	 */
 	if maxsize < 256 {
